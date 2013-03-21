1758a354e5665b310c65e25c7762c893b78b8453
Xavier Cambar <xcambar@gmail.com>
2013-03-21 15:47:00 +0100

    Removes unused widget

--- app/scripts/templates.js
+++ app/scripts/templates.js
@@ -38,31 +38,3 @@ helpers = helpers || Handlebars.helpers; data = data || {};
 
   return "\n";
   });
-
-this["Hull"]["templates"]["images/main"] = Handlebars.template(function (Handlebars,depth0,helpers,partials,data) {
-  this.compilerInfo = [2,'>= 1.0.0-rc.3'];
-helpers = helpers || Handlebars.helpers; data = data || {};
-  var buffer = "", stack1, options, helperMissing=helpers.helperMissing, escapeExpression=this.escapeExpression, functionType="function", self=this, blockHelperMissing=helpers.blockHelperMissing;
-
-function program1(depth0,data) {
-  
-  var buffer = "", stack1, stack2, options;
-  buffer += "\n  <div style=\"float:left\">\n    <img src=";
-  options = {hash:{},data:data};
-  buffer += escapeExpression(((stack1 = helpers.imageUrl),stack1 ? stack1.call(depth0, depth0.id, options) : helperMissing.call(depth0, "imageUrl", depth0.id, options)))
-    + "/><br>\n    <a href=\"#\" data-hull-action=\"delete\" data-hull-id=\"";
-  if (stack2 = helpers.id) { stack2 = stack2.call(depth0, {hash:{},data:data}); }
-  else { stack2 = depth0.id; stack2 = typeof stack2 === functionType ? stack2.apply(depth0) : stack2; }
-  buffer += escapeExpression(stack2)
-    + "\">Delete</a>\n  </div>\n";
-  return buffer;
-  }
-
-  options = {hash:{},inverse:self.noop,fn:self.program(1, program1, data),data:data};
-  if (stack1 = helpers.images) { stack1 = stack1.call(depth0, options); }
-  else { stack1 = depth0.images; stack1 = typeof stack1 === functionType ? stack1.apply(depth0) : stack1; }
-  if (!helpers.images) { stack1 = blockHelperMissing.call(depth0, stack1, options); }
-  if(stack1 || stack1 === 0) { buffer += stack1; }
-  buffer += "\n";
-  return buffer;
-  });
