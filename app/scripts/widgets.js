f0ff0e49e13c427d29672b37a3861efbf367f24e
Xavier Cambar <xcambar@gmail.com>
2013-03-22 14:33:27 +0100

    Fixes loggedIn status

--- app/scripts/widgets.js
+++ app/scripts/widgets.js
@@ -2,13 +2,15 @@ Hull.widget('hullstache', {
   templates: ['loggedIn', 'notLogged'],
   refreshEvents: ['model.hull.me.change'],
   beforeRender: function (data) {
-    this.template = this.loggedIn() ? 'loggedIn' : 'notLogged';
-    if (this.loggedIn()) {
+    "use strict";
+    this.template = this.loggedIn('facebook') ? 'loggedIn' : 'notLogged';
+    if (this.loggedIn('facebook')) {
       data.picture = encodeURIComponent(data.me.identities[0].picture.replace('type=square', 'type=large'));
     }
   },
   actions: {
     sendByMail: function (elt, evt, data) {
+      "use strict";
       var imgSrc = this.$el.find('#hull_stache').attr('src');
       evt.preventDefault();
       this.api.post('me/images', {name: "hull-stache", source_url: imgSrc}).then(function (obj) {
